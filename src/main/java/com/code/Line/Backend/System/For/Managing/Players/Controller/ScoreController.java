4e6eef1b7df525410dd9a827446ac2fe42238da6
israsaif <1616080@mazcol.edu.om>
2023-07-09 15:13:13 +0400

    Add Score Controller

---
+++ src/main/java/com/code/Line/Backend/System/For/Managing/Players/Controller/ScoreController.java
@@ -0,0 +1,9 @@
+package com.code.Line.Backend.System.For.Managing.Players.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/s1/Score")
+public class ScoreController {
+}
