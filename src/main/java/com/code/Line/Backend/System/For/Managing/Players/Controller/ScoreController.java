d8d16d2581c6d26ad751799a3e8f377bcbd9234a
israsaif <1616080@mazcol.edu.om>
2023-07-10 08:45:12 +0400

    Add Autowired Score Service

--- src/main/java/com/code/Line/Backend/System/For/Managing/Players/Controller/ScoreController.java
+++ src/main/java/com/code/Line/Backend/System/For/Managing/Players/Controller/ScoreController.java
@@ -1,9 +1,14 @@
 package com.code.Line.Backend.System.For.Managing.Players.Controller;
 
+import com.code.Line.Backend.System.For.Managing.Players.Services.ScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "api/s1/Score")
 public class ScoreController {
+    @Autowired
+    ScoreService scoreService;
+
 }
