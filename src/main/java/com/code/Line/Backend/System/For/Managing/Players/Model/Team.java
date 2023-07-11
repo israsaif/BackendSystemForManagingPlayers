14b2ed5f9a5dee10fafe7951b887b5084b5f8413
israsaif <1616080@mazcol.edu.om>
2023-07-11 13:01:05 +0400

    Add class Team

---
+++ src/main/java/com/code/Line/Backend/System/For/Managing/Players/Model/Team.java
@@ -0,0 +1,21 @@
+package com.code.Line.Backend.System.For.Managing.Players.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Setter
+@Getter
+@Data
+@Entity
+public class Team  extends BaseEntity{
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    public String teamName;
+    public List<Player> players;
+}
